db9530c2958e792830b66fc3a222ba5f9576243c
nullkaaryle <pinni@Karhunsammal.local>
2017-08-13 02:35:12 +0300

    parannettu reseptimallin hakuja

--- app/models/resepti.php
+++ app/models/resepti.php
@@ -9,39 +9,68 @@ class Resepti extends BaseModel{
     }
 
     public static function all(){
-        $query = DB::connection()->prepare('SELECT * FROM Resepti');
+        $query = DB::connection()->prepare('SELECT r.id, 
+                                                    ap.nimi AS apteekki, 
+                                                    po.sukunimi AS potilas_sukunimi, 
+                                                    po.etunimi AS potilas_etunimi, 
+                                                    li.sukunimi AS laakari_sukunimi, 
+                                                    li.etunimi AS laakari_etunimi, 
+                                                    le.tuotenimi AS laake, 
+                                                    r.ohje, 
+                                                    r.paivamaara
+                                            FROM Resepti r
+                                            INNER JOIN Apteekki ap ON r.apteekki = ap.id
+                                            INNER JOIN Potilas po ON r.potilas = po.id
+                                            INNER JOIN Laakari li ON r.laakari = li.id
+                                            INNER JOIN Laake le ON r.laake = le.id');
+
         $query->execute();
         $rows = $query->fetchAll();
         $reseptit = array();
 
         foreach($rows as $row){
             $reseptit[] = new Resepti(array(
-                'id' => $row['id'],
-                'apteekki' => $row['apteekki'],
-                'potilas' => $row['potilas'],
-                'laakari' => $row['laakari'],
-                'laake' => $row['laake'],
-                'ohje' => $row['ohje'],
-                'paivamaara' => $row['paivamaara']
+                'id'        =>  $row['id'],
+                'apteekki'  =>  $row['apteekki'],
+                'potilas'   =>  $row['potilas_sukunimi'] . ', ' . $row['potilas_etunimi'],
+                'laakari'   =>  $row['laakari_sukunimi'] . ', ' . $row['laakari_etunimi'],
+                'laake'     =>  $row['laake'],
+                'ohje'      =>  $row['ohje'],
+                'paivamaara' =>  $row['paivamaara']
             ));
         }
 
         return $reseptit;
     }
 
+
     public static function find($id){
-        $query = DB::connection()->prepare('SELECT * FROM Resepti WHERE id = :id LIMIT 1');
+        $query = DB::connection()->prepare('SELECT r.id, 
+                                                    ap.nimi AS apteekki, 
+                                                    po.sukunimi AS potilas_sukunimi, 
+                                                    po.etunimi AS potilas_etunimi, 
+                                                    li.sukunimi AS laakari_sukunimi, 
+                                                    li.etunimi AS laakari_etunimi, 
+                                                    le.tuotenimi AS laake, 
+                                                    r.ohje, 
+                                                    r.paivamaara
+                                            FROM Resepti r
+                                            INNER JOIN Apteekki ap ON r.apteekki = ap.id
+                                            INNER JOIN Potilas po ON r.potilas = po.id
+                                            INNER JOIN Laakari li ON r.laakari = li.id
+                                            INNER JOIN Laake le ON r.laake = le.id
+                                            WHERE r.id = :id LIMIT 1');
         $query->execute(array('id' => $id));
         $row = $query->fetch();
 
         if($row){
             $resepti = new Resepti(array(
-                'id' => $row['id'],
-                'apteekki' => $row['apteekki'],
-                'potilas' => $row['potilas'],
-                'laakari' => $row['laakari'],
-                'laake' => $row['laake'],
-                'ohje' => $row['ohje'],
+                'id'        => $row['id'],
+                'apteekki'  =>  $row['apteekki'],
+                'potilas'   =>  $row['potilas_sukunimi'] . ', ' . $row['potilas_etunimi'],
+                'laakari'   =>  $row['laakari_sukunimi'] . ', ' . $row['laakari_etunimi'],
+                'laake'     =>  $row['laake'],
+                'ohje'      =>  $row['ohje'],
                 'paivamaara' => $row['paivamaara']
             ));
 
